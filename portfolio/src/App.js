7d925ad62069fdef47462c3382426017695fd23f
Thomas Martin <161907136+TommyMart@users.noreply.github.com>
2025-01-15 14:00:15 +1030

    feat: header implemented

---
+++ portfolio/src/App.js
@@ -0,0 +1,15 @@
+import "./App.css";
+import { ChakraProvider } from "@chakra-ui/react";
+import Header from "./components/Header";
+
+function App() {
+  return (
+    <ChakraProvider>
+      <main>
+        <Header />
+      </main>
+    </ChakraProvider>
+  );
+}
+
+export default App;
