2ddc25f1fc86755faf67979da17f73937c50eefd
Thomas Martin <161907136+TommyMart@users.noreply.github.com>
2025-01-15 14:37:57 +1030

    feat: landing page

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,12 +1,14 @@
 import "./App.css";
 import { ChakraProvider } from "@chakra-ui/react";
 import Header from "./components/Header";
+import LandingSection from "./components/LandingSection";
 
 function App() {
   return (
     <ChakraProvider>
       <main>
         <Header />
+        <LandingSection />
       </main>
     </ChakraProvider>
   );
---
+++ portfolio/src/components/LandingSection.js
@@ -0,0 +1,27 @@
+import { Avatar, Heading, VStack } from "@chakra-ui/react";
+import FullScreenSection from "./FullScreenSection";
+
+const greeting = "Hello, I am Tom!";
+const bio1 = "A Junior Full-Stack Developer";
+
+
+const LandingSection = () => {
+
+    return (
+    <FullScreenSection
+        justifyContent="center"
+        alignItems="center"
+        isDarkBackground
+        backgroundColor="#2A4365"
+    >
+        <Avatar src={"/IMG_0900.jpeg"}/>
+        <VStack>
+            <h1>{greeting}</h1>
+            <Heading>{bio1}</Heading>
+            
+        </VStack>
+    </FullScreenSection>
+    )
+}
+
+export default LandingSection;
