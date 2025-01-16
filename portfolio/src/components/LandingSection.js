559837fb4e8c657f94ebf85f93ebdd215e0bcb89
Thomas Martin <161907136+TommyMart@users.noreply.github.com>
2025-01-16 18:06:06 +1030

    feat: added a responsive transcription button that shows text

--- portfolio/src/components/LandingSection.js
+++ portfolio/src/components/LandingSection.js
@@ -1,4 +1,4 @@
-import { Avatar, Button, Heading, VStack } from "@chakra-ui/react";
+import { Avatar, Button, Heading, VStack, Text } from "@chakra-ui/react";
 import FullScreenSection from "./FullScreenSection";
 import AudioIntro from "./intro";
 import DisplayIcons from "./iconDisplay";
@@ -8,6 +8,7 @@ const greeting = "Hello, I am Tom!";
 const bio1 = "A Junior Full-Stack Developer";
 
 const LandingSection = () => {
+    const [showTranscription, setShowTranscription] = useState(false);
 
     const [iconDelay1, setIconDelay1] = useState(false);
     const [iconDelay2, setIconDelay2] = useState(false);
@@ -37,14 +38,28 @@ const LandingSection = () => {
         {iconDelay2 && <DisplayIcons />}
         <DisplayIcons />
         
-        <Avatar position="relative" zIndex={10} className="expand2 glow" src={"/IMG_0900.jpeg"} width={300} height={300}/>
-        <VStack py={20}>
+        <Avatar position="relative" mt={118} zIndex={10} className="expand2 glow" src={"/IMG_0900.jpeg"} width={300} height={300}/>
+        <VStack pt={16} pb={4}>
             <h1 position="relative" zIndex={10}>{greeting}</h1>
             <Heading position="relative" zIndex={10} className="expand2" >{bio1}</Heading>
             <DisplayIcons />
         </VStack>
-        <AudioIntro />
+        <AudioIntro pt={0}/>
         <DisplayIcons />
+        <Button position="relative" zIndex={10} colorScheme="purple" 
+        onClick={() => setShowTranscription(!showTranscription)}>
+            Transcription</Button>
+        {showTranscription && (<Text py={10} width="60%" textAlign={"center"}>
+        "Hi 👋, my name's Tom and welcome to my Portfolio Website! <br/><br/>
+
+I didn’t grow up with computers, but over the past year, I’ve become deeply fascinated with the world of technology. What started as a curiosity turned into a passion that drives me to spend every moment learning, building, and experimenting. From coding challenges to full-stack projects, I’ve immersed myself in this incredible field, constantly pushing myself to grow and improve. <br/><br/>
+
+This portfolio showcases my journey so far, highlighting the projects I’ve created and the skills I’ve developed along the way. Each piece represents not just a technical achievement but also my dedication to problem-solving, creativity, and continuous learning. <br/><br/>
+
+Thank you for visiting, and I hope my work inspires you as much as this journey has inspired me!"
+       
+        </Text>)
+}
     </FullScreenSection>
     </>
     )
